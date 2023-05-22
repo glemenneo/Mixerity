cdb9d2741f6c15c630edb3f9539da4bf4cec58e2
Glemen Neo <glemenneo@gmail.com>
2023-05-22 11:16:09 +0800

    Create user entity

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,40 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('increment')
+    uid: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Exclude()
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @Exclude()
+    @Column({ nullable: true })
+    refreshToken: string;
+
+    // Soft delete column
+    // @Exclude()
+    // @DeleteDateColumn({ nullable: true })
+    // deletedDate: Date;
+
+    constructor(partial: Partial<User>) {
+        Object.assign(this, partial);
+    }
+}
