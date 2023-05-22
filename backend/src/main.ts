5bafca0730c88514f41b2f51fbd834b0eb78e33a
Glemen Neo <glemen.neo@flex-solver.com.sg>
2023-05-22 10:42:05 +0800

    Add config service module for env files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+
+    const configService = app.get(ConfigService);
+    const PORT = configService.get<string>('PORT') || 3000;
+
+    await app.listen(PORT, () => {
+        console.log(`NestJS server running on port ${PORT}.`);
+    });
 }
 bootstrap();
