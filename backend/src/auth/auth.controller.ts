f016a299156ef8d278d897118fc95e0498f12713
Glemen Neo <glemenneo@gmail.com>
2023-05-22 14:27:39 +0800

    Add basic routes for authentication

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    UseInterceptors,
+    ClassSerializerInterceptor,
+    Post,
+    Body,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from 'src/users/entities/user.entity';
+import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+
+    @Post('/register')
+    register(@Body() body: any): Promise<User> {
+        const { email, username, password } = body;
+        return this.authService.register(email, username, password);
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('/login')
+    login() {}
+
+    @Post('/logout')
+    logout() {}
 }
