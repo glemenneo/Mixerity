4c87d3a0f532ea2523158616d395c2b55e8c0799
Glemen Neo <glemenneo@gmail.com>
2023-05-22 11:00:27 +0800

    Setup typeorm in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -11,6 +12,21 @@ import { UsersModule } from './users/users.module';
                     ? '.env'
                     : `.env.${process.env.NODE_ENV}`,
             ],
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('DB_HOST'),
+                port: configService.get('DB_PORT'),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_NAME'),
+                entities: [],
+                autoLoadEntities: true,
+                synchronize: process.env.NODE_ENV !== 'production',
+            }),
+            inject: [ConfigService],
         }),
         UsersModule,
     ],
