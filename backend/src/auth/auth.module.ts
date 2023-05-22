132cf0395778a260feb6b5a8e24eb56dde92383a
Glemen Neo <glemenneo@gmail.com>
2023-05-22 14:34:40 +0800

    Add Jwt import

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-    imports: [UsersModule, PassportModule],
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({ useFactory: async () => ({}) }),
+    ],
     controllers: [AuthController],
     providers: [AuthService, LocalStrategy],
 })
