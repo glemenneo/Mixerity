2de690b0e3b0960ab491b62fd6927fbec7ef8ef3
Glemen Neo <glemenneo@gmail.com>
2023-05-22 18:07:28 +0800

    feat: add validation pipe for profile module

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
@@ -7,7 +8,15 @@ import { Profile } from './entities/profile.entity';
 @Module({
     imports: [TypeOrmModule.forFeature([Profile])],
     controllers: [ProfileController],
-    providers: [ProfileService],
+    providers: [
+        ProfileService,
+        {
+            provide: APP_PIPE,
+            useValue: new ValidationPipe({
+                whitelist: true,
+            }),
+        },
+    ],
     exports: [ProfileService],
 })
 export class ProfileModule {}
