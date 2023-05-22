6e89d7049955f1a12383222759f4dc9c6f219f14
Yun Ru <yunruuchin@gmail.com>
2023-05-22 17:33:31 +0800

    feat: add login page view

---
+++ frontend/src/components/auth/LoginView.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="login-view">
+    <h1>Login</h1>
+    <el-form
+      :model="authForm"
+      :rules="rules"
+      status-icon
+      id="login-form"
+    >
+      <el-form-item
+        label="Username"
+        prop="username"
+      >
+        <el-input
+          v-model="authForm.username"
+          placeholder="Username"
+          id="Username"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="Password"
+        prop="password"
+      >
+        <el-input
+          type="password"
+          v-model="authForm.password"
+          autocomplete="off"
+          show-password
+          placeholder="Password"
+          id="Password"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+    <div class="login-register-buttons">
+      <el-button
+        type="primary"
+        @click="handleLoginButtonClick"
+        style="background-color: #f43f5e"
+        icon="el-icon-position"
+      >
+        Login
+      </el-button>
+      <el-button
+        type="text"
+        @click="handleRegisterButtonClick"
+        style="color: #f43f5e"
+      >
+        No account? Register here now!
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { AuthActions } from "@/store/modules/AuthModule";
+
+@Component({
+  name: "LoginView",
+})
+export default class extends Vue {
+  authForm = {
+    username: "",
+    password: "",
+  };
+  rules = {
+    username: [
+      {
+        required: true,
+        message: "Please input username",
+        trigger: "blur",
+      },
+      {
+        validator: (
+          rule: any,
+          value: string,
+          callback: (arg0: Error | undefined) => void
+        ) => {
+          this.validateUsername(rule, value, callback);
+        },
+        trigger: "blur",
+      },
+    ],
+    password: [
+      {
+        required: true,
+        message: "Please input password",
+        trigger: "blur",
+      },
+    ],
+  };
+
+  validateUsername = (
+    rule: any,
+    value: string,
+    callback: (arg0: Error | undefined) => void
+  ) => {
+    if (!value) {
+      callback(new Error("Please input username"));
+    } else {
+      if (value.length < 6) {
+        callback(new Error("Username should be at least 6 characters"));
+      } else {
+        callback(undefined);
+      }
+    }
+  };
+
+  handleLoginButtonClick(): void {
+    const auth1 = this.authForm;
+    this.$store.dispatch(`auth/${AuthActions.LOGIN_USER}`, auth1);
+    // Todo: sync with backend
+    // On success response
+    setTimeout(() => this.$router.push(`/posts`), 1000);
+  }
+
+  handleRegisterButtonClick(): void {
+    this.$router.push(`/register`);
+  }
+}
+</script>
+
+<style scoped>
+.login-view {
+  height: 100vh;
+}
+.login-register-buttons {
+  display: flex;
+  justify-content: center;
+}
+.el-button {
+  margin: 10px 20px;
+  border: none;
+}
+</style>
