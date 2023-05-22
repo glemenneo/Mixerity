d6c1c07149d4d671187fd89a892794317038be39
Yun Ru <yunruuchin@gmail.com>
2023-05-22 16:49:46 +0800

    feat: add page not found page

---
+++ frontend/src/pages/NotFoundPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-view">
+    <div>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <el-button
+        type="primary"
+        @click="handleBackButtonClick"
+        style="background-color: #f43f5e"
+        icon="el-icon-position"
+      >
+        Go back
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "NotFoundPage",
+})
+export default class extends Vue {
+  handleBackButtonClick(): void {
+    this.$router.push("/");
+  }
+}
+</script>
