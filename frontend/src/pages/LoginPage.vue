c5dc025006856192d6ab3b90100afc129a1252c1
Yun Ru <yunruuchin@gmail.com>
2023-05-22 17:35:32 +0800

    feat: add login and register pages

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-view">
+    <LoginView />
+  </div>
+</template>
+
+<script lang="ts">
+import LoginView from "@/components/auth/LoginView.vue";
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "LoginPage",
+  components: {
+    LoginView,
+  },
+})
+export default class extends Vue {}
+</script>
---
+++ frontend/src/pages/RegisterPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-view">
+    <RegisterView />
+  </div>
+</template>
+
+<script lang="ts">
+import RegisterView from "@/components/auth/RegisterView.vue";
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "LoginPage",
+  components: {
+    RegisterView,
+  },
+})
+export default class extends Vue {}
+</script>
