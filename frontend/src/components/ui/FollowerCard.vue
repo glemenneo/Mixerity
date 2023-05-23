859820a7b96a12695a25acceae8a0153351809a7
Yun Ru <yunruuchin@gmail.com>
2023-05-23 17:48:30 +0800

    feat: add card to display follower's information

---
+++ frontend/src/components/ui/FollowerCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="follower-card">
+    {{ username }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "FollowerCard",
+})
+export default class extends Vue {
+  @Prop() username!: string;
+
+  mounted() {
+    this.username = "John Doe";
+  }
+}
+</script>
+
+<style scoped>
+.follower-card {
+  width: 80vw;
+  background-color: white;
+}
+</style>
