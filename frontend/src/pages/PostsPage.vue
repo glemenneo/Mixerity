1c070018134d2c959ca2c7256b79be6c2325cf57
Yun Ru <yunruuchin@gmail.com>
2023-05-23 14:34:25 +0800

    feat: add posts page

---
+++ frontend/src/pages/PostsPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-view posts-page">
+    <el-container>
+      <el-header>
+        <h1>For You Page</h1>
+      </el-header>
+    </el-container>
+    <PostList />
+  </div>
+</template>
+
+<script lang="ts">
+import PostList from "@/components/posts/PostList.vue";
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "ForYouPage",
+  components: {
+    PostList,
+  },
+})
+export default class extends Vue {}
+</script>
+
+<style scoped>
+.posts-page {
+  height: max-content;
+  background: #f8fafc;
+}
+</style>
