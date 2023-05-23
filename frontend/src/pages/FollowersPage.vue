e5105285faa9059ca6607f0751d4db8686c178d1
Yun Ru <yunruuchin@gmail.com>
2023-05-23 15:24:38 +0800

    style: add followers page header styling

--- frontend/src/pages/FollowersPage.vue
+++ frontend/src/pages/FollowersPage.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="page-view">
-    <h1>Followers Page</h1>
+  <div class="page-view followers-page">
+    <span class="header">Followers</span>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "FollowersPage",
+})
+export default class extends Vue {}
+</script>
+
+<style scoped>
+.followers-page {
+  height: max-content;
+  background: #f8fafc;
+}
+.header {
+  font-size: 2rem;
+  font-weight: 600;
+  margin: 0;
+  padding: 0;
+  color: #cbd5e1;
+}
+</style>
