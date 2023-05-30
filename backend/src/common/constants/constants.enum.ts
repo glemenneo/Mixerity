a0f8db4c933b2f561879c21e3cc1d05408791a9a
Glemen Neo <glemenneo@gmail.com>
2023-05-30 17:25:16 +0800

    feat: add enums for columns of user validation

--- backend/src/common/constants/constants.enum.ts
+++ backend/src/common/constants/constants.enum.ts
@@ -2,3 +2,10 @@ export enum OrderBy {
     ASC = 'ASC',
     DESC = 'DESC',
 }
+
+export enum UserColumns {
+    UID = 'uid',
+    EMAIL = 'email',
+    USERNAME = 'username',
+    CREATED = 'created',
+}
