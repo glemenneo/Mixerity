464bbed3f89643510d60aac4804168a63bcf4139
Glemen Neo <glemenneo@gmail.com>
2023-05-31 15:36:57 +0800

    feat: generate posts module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
     imports: [
@@ -33,6 +34,7 @@ import { ProfileModule } from './profile/profile.module';
         UsersModule,
         AuthModule,
         ProfileModule,
+        PostsModule,
     ],
 })
 export class AppModule {}
