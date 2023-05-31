62fa4d6e87898d44d3c4cd9834c0009e3c79c513
Glemen Neo <glemenneo@gmail.com>
2023-05-31 15:21:20 +0800

    feat: use helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.use(cookieParser());
+    app.use(helmet());
 
     const configService = app.get(ConfigService);
     const PORT = configService.get<string>('PORT') || 3000;
